3fd39f0a9ee4564556fa2a3bc436317936af1649
Tejas Balamukesh <tejasbalamukesh@gmail.com>
2026-03-28 12:43:15 +0530

    Debug

--- transactions_mobile/lib/services/api_config.dart
+++ transactions_mobile/lib/services/api_config.dart
@@ -2,6 +2,6 @@ class ApiConfig {
     // Base URL for the backend API.
     // IMPORTANT: If running on an android emulator, change 'localhost' to '10.2.2.2'
     // to successfully connect to your local computer's backend. 
-    // static const String baseUrl = "http://10.2.2.2:8000";
+    // static const String baseUrl = "http://10.0.2.2:8000";
     static const String baseUrl = "http://localhost:8000";
 }
